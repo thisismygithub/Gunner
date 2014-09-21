4d8a7f5dc84f0380fd23a284a5bfb76daa45267e
marsen <thisismysoul@gmail.com>
2014-09-22 03:05:07 +0800

    Marsen web site by MVC5

---
+++ Gunner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gunner.Startup))]
+namespace Gunner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
