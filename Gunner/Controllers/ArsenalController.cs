dd2f8f0334026e5c855651d397fa203aa536a357
marsen <thisismysoul@gmail.com>
2014-09-24 07:35:29 +0800

    Gunner MVC
    Gunner

---
+++ Gunner/Controllers/ArsenalController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gunner.Models;
+
+namespace Gunner.Controllers
+{
+    public class ArsenalController : Controller
+    {
+        private ArsenalDbContext db = new ArsenalDbContext();
+        // GET: Arsenal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gunner/Models/Arsenal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Gunner.Models
+{
+    public class Fixtures
+    {
+        public int Id { get; set; }
+        public string Opponent { get; set; }
+        public DateTime Date { get; set; }
+        public string Result { get; set; }
+        public string Ground { get; set; }
+        public string Stadium { get; set; }
+        public int Attendance { get; set; }
+        public string Referee { get; set; }
+    }
+
+    public class ArsenalDbContext : DbContext
+    {
+        public DbSet<Fixtures> Fixtures { get; set; }
+    }
+
+}
