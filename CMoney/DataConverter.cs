5d6b87110d4d51c7bd03a377ff3a3298a6779592
lin <thisismysoul@gmail.com>
2015-05-04 07:54:22 +0800

    Change Web Solution

---
+++ CMoney/DataConverter.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+
+public class DataConverter
+{
+    /// <summary>
+    /// Convert a DataTable into an XML string.
+    /// </summary>
+    /// <param name="dataTable">DataTable</param>
+    /// <param name="strTableName">Table Name</param>
+    /// <returns></returns>
+    public static string ToXmlString(DataTable dataTable, string strTableName)
+    {
+        if (dataTable == null)
+        {
+            return "dataTable is null.";
+        }
+
+        XmlDocument xmlDoc = ToXmlDocument(dataTable, strTableName);
+        return xmlDoc.InnerXml;
+    }
+
+    /// <summary>
+    /// Convert a DataSet into an XML string.
+    /// </summary>
+    /// <param name="dataSet">DataSet</param>
+    /// <param name="strTableName">Table Name</param>
+    /// <returns></returns>
+    public static string ToXmlString(DataSet dataSet, string strTableName)
+    {
+        if (dataSet == null) return "dataSet is null.";
+
+        XmlDocument xmlDoc;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < dataSet.Tables.Count; ++i)
+        {
+            xmlDoc = ToXmlDocument(dataSet.Tables[i], strTableName + i.ToString());
+            sb.Append(xmlDoc.InnerXml);
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Convert a DataTable into an XML document.
+    /// </summary>
+    /// <param name="dataTable">DataTable</param>
+    /// <param name="strTableName">Table Name</param>
+    /// <returns></returns>
+    public static XmlDocument ToXmlDocument(DataTable dataTable, string strTableName)
+    {
+        StringWriter stringWriter = new StringWriter();
+        XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
+        dataTable.TableName = strTableName;
+        dataTable.WriteXml(xmlTextWriter, XmlWriteMode.IgnoreSchema);
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(stringWriter.ToString());
+
+        return xmlDoc;
+    }
+
+    public static string Serialize(object value)
+    {
+        Type type = value.GetType();
+
+        Newtonsoft.Json.JsonSerializer json = new Newtonsoft.Json.JsonSerializer();
+
+        json.NullValueHandling = NullValueHandling.Ignore;
+
+        json.ObjectCreationHandling = Newtonsoft.Json.ObjectCreationHandling.Replace;
+        json.MissingMemberHandling = Newtonsoft.Json.MissingMemberHandling.Ignore;
+        json.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+        if (type == typeof(DataRow))
+        {
+            json.Converters.Add(new DataRowConverter());
+        }
+        else if (type == typeof(DataTable))
+        {
+            json.Converters.Add(new DataTableConverter());
+        }
+        else if (type == typeof(DataSet))
+        {
+            json.Converters.Add(new DataSetConverter());
+        }
+        string output;
+        using (StringWriter sw = new StringWriter())
+        using (Newtonsoft.Json.JsonTextWriter writer = new JsonTextWriter(sw))
+        {
+            writer.QuoteChar = '"';
+            json.Serialize(writer, value);
+
+            output = sw.ToString();
+            writer.Close();
+            sw.Close();
+        }
+
+        return output;
+    }
+
+    public static object Deserialize(string jsonText, Type valueType)
+    {
+        Newtonsoft.Json.JsonSerializer json = new Newtonsoft.Json.JsonSerializer();
+
+        json.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+        json.ObjectCreationHandling = Newtonsoft.Json.ObjectCreationHandling.Replace;
+        json.MissingMemberHandling = Newtonsoft.Json.MissingMemberHandling.Ignore;
+        json.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+        StringReader sr = new StringReader(jsonText);
+        Newtonsoft.Json.JsonTextReader reader = new JsonTextReader(sr);
+        object result = json.Deserialize(reader, valueType);
+        reader.Close();
+
+        return result;
+    }
+
+    public static T DeserializeObject<T>(string jsonText)
+    {
+        return JsonConvert.DeserializeObject<T>(jsonText);
+    }
+
+    /// <summary>
+    /// Converts a <see cref="DataRow"/> object to and from JSON.
+    /// </summary>
+    public class DataRowConverter : JsonConverter
+    {
+        public DataRowConverter()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        public override void WriteJson(JsonWriter writer, object dataRow, JsonSerializer ser)
+        {
+            DataRow row = dataRow as DataRow;
+
+            // *** HACK: need to use root serializer to write the column value
+            //     should be fixed in next ver of JSON.NET with writer.Serialize(object)
+            //JsonSerializer ser = new JsonSerializer();
+
+            writer.WriteStartObject();
+            foreach (DataColumn column in row.Table.Columns)
+            {
+                writer.WritePropertyName(column.ColumnName);
+                ser.Serialize(writer, row[column]);
+            }
+            writer.WriteEndObject();
+        }
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified value type.
+        /// </summary>
+        /// <param name="valueType">Type of the value.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can convert the specified value type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type valueType)
+        {
+            return typeof(DataRow).IsAssignableFrom(valueType);
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer ser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Converts a DataTable to JSON. Note no support for deserialization
+    /// </summary>
+    public class DataTableConverter : JsonConverter
+    {
+        public DataTableConverter()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        public override void WriteJson(JsonWriter writer, object dataTable, JsonSerializer ser)
+        {
+            DataTable table = dataTable as DataTable;
+            DataRowConverter converter = new DataRowConverter();
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("Rows");
+            writer.WriteStartArray();
+
+            foreach (DataRow row in table.Rows)
+            {
+                converter.WriteJson(writer, row, ser);
+            }
+
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified value type.
+        /// </summary>
+        /// <param name="valueType">Type of the value.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can convert the specified value type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type valueType)
+        {
+            return typeof(DataTable).IsAssignableFrom(valueType);
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer ser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Converts a <see cref="DataSet"/> object to JSON. No support for reading.
+    /// </summary>
+    public class DataSetConverter : JsonConverter
+    {
+        public DataSetConverter()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        public override void WriteJson(JsonWriter writer, object dataset, JsonSerializer ser)
+        {
+            DataSet dataSet = dataset as DataSet;
+
+            DataTableConverter converter = new DataTableConverter();
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("Tables");
+            writer.WriteStartArray();
+
+            foreach (DataTable table in dataSet.Tables)
+            {
+                converter.WriteJson(writer, table, ser);
+            }
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified value type.
+        /// </summary>
+        /// <param name="valueType">Type of the value.</param>
+        /// <returns>
+        ///     <c>true</c> if this instance can convert the specified value type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type valueType)
+        {
+            return typeof(DataSet).IsAssignableFrom(valueType);
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer ser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CMoney/WebAccess.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+using System.Web;
+
+public class WebAccess
+{
+    public string Url { get; set; }
+    public string FormName { get; set; }
+    private NameValueCollection _dataSet = new NameValueCollection();
+
+    /// <summary>
+    /// Creates a new instance of the WebAccess class
+    /// </summary>
+    public WebAccess()
+    {
+        Url = string.Empty;
+        FormName = "form1";
+    }
+
+    public WebAccess(string url, string formName = "form1")
+    {
+        Url = url;
+        FormName = formName;
+    }
+
+    /// <summary>
+    /// Adds data to be submitted.
+    /// </summary>
+    public void AddData(string name, string value)
+    {
+        _dataSet.Add(name, value);
+    }
+
+    /// <summary>
+    /// Load page content.
+    /// </summary>
+    /// <param name="timeout"></param>
+    /// <returns>Page content in HTML</returns>
+    public string LoadAtServer(int timeout = 120000)
+    {
+        string result = string.Empty;
+
+        try
+        {
+            HttpWebRequest hwRequest = (HttpWebRequest)HttpWebRequest.Create(Url);
+            hwRequest.Timeout = timeout;
+            HttpWebResponse hwResponse = (HttpWebResponse)hwRequest.GetResponse();
+            Stream myStream = hwResponse.GetResponseStream();
+            StreamReader sr = new StreamReader(myStream, Encoding.UTF8);
+            StringBuilder strBuilder = new StringBuilder();
+            while (-1 != sr.Peek())
+            {
+                strBuilder.Append(sr.ReadLine());
+            }
+
+            result = strBuilder.ToString();
+        }
+        catch
+        {
+            // CMoney.Logger.Log(ex); might not be set up, so do nothing.
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Load page content.
+    /// </summary>
+    /// <param name="url"> </param>
+    /// <param name="timeout"></param>
+    /// <returns>Page content in HTML</returns>
+    public static string LoadAtServer(string url, int timeout = 120000)
+    {
+        string result = string.Empty;
+
+        try
+        {
+            var hwRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            hwRequest.Timeout = timeout;
+            var hwResponse = (HttpWebResponse)hwRequest.GetResponse();
+            var myStream = hwResponse.GetResponseStream();
+            var sr = new StreamReader(myStream, Encoding.UTF8);
+            var strBuilder = new StringBuilder();
+            while (-1 != sr.Peek())
+            {
+                strBuilder.Append(sr.ReadLine());
+            }
+
+            result = strBuilder.ToString();
+        }
+        catch
+        {
+            // CMoney.Logger.Log(ex); might not be set up, so do nothing.
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Submit data at client-side.
+    /// The target URL can be a relative URL.
+    /// </summary>
+    public void SubmitAtClient()
+    {
+        var context = HttpContext.Current;
+        context.Response.Clear();
+        context.Response.Write("<html><head>");
+        context.Response.Write(string.Format("</head><body onload=\"document.{0}.submit()\">", FormName));
+        context.Response.Write(string.Format("<form name=\"{0}\" method=\"{1}\" action=\"{2}\" target=\"_self\">", FormName, "POST", Url));
+        for (int i = 0; i < _dataSet.Keys.Count; i++)
+        {
+            context.Response.Write(string.Format("<input name=\"{0}\" type=\"hidden\" value=\"{1}\">", HttpUtility.HtmlEncode(_dataSet.Keys[i]), HttpUtility.HtmlEncode(_dataSet[_dataSet.Keys[i]])));
+        }
+        context.Response.Write("</form>");
+        context.Response.Write("</body></html>");
+        //HttpContext.Current.ApplicationInstance.CompleteRequest();
+        context.Response.End();
+    }
+
+    /// <summary>
+    /// Submit data at server-side.
+    /// The target URL must be an absolute URL that starts with http:// (https might have certificate problem).
+    /// </summary>
+    public string SubmitAtServer()
+    {
+        var result = string.Empty;
+
+        try
+        {
+            HttpWebRequest hwRequest = (HttpWebRequest)WebRequest.Create(Url);
+            hwRequest.Method = "POST";
+            hwRequest.ContentType = "application/x-www-form-urlencoded";
+
+            string parameters = GenParameters();
+
+            byte[] buffer = Encoding.UTF8.GetBytes(parameters);
+            hwRequest.ContentLength = buffer.Length;
+            var postData = hwRequest.GetRequestStream();
+            postData.Write(buffer, 0, buffer.Length);
+            postData.Close();
+
+            var hwResponse = (HttpWebResponse)hwRequest.GetResponse();
+            var stream = hwResponse.GetResponseStream();
+            var streamReader = new StreamReader(stream);
+            result = streamReader.ReadToEnd();
+        }
+        catch (Exception ex)
+        {
+            // CMoney.Logger.Log(ex);
+            result = ex.ToString();
+        }
+
+        return result;
+    }
+
+    private string GenParameters()
+    {
+        var result = string.Empty;
+        //歐付寶檢查碼機制:將傳遞參數依照第一個英文字母，由 A 到 Z 的順序來排序(遇到第一個英名字母相同時，以第二
+        //個英名字母來比較，以此類推) ，並且以&方式將所有參數串連
+        var sortedDataset = _dataSet.AllKeys.OrderBy(k => k);
+        foreach (var item in sortedDataset)
+        {
+            result += string.Format("{0}={1}", item, _dataSet[item]);
+            if (sortedDataset.Last() != item)
+            {
+                result += "&";
+            }
+        }
+
+        return result;
+    }
+
+    public NameValueCollection GetUrlParameters()
+    {
+        return _dataSet;
+    }
+}
