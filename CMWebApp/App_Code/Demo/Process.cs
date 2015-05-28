5451c3f08cf347bfca3017303cdcf5dc97036993
Marsen Lin <thisismysoul@gmail.com>
2015-05-28 16:39:42 +0800

    demo upload  ver1
    upload img

--- CMWebApp/App_Code/Demo/Process.cs
+++ CMWebApp/App_Code/Demo/Process.cs
@@ -1,7 +1,10 @@
 ﻿
 // Demo Namespace底下，只放著demo用的class 
 
+using System;
 using System.Collections.Specialized;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Web;
 
 namespace Demo
@@ -25,6 +28,10 @@ namespace Demo
                 case "getpagerdata":
                     response = GetPagerData();                    
                     break;
+
+                case "uploadimg":
+                    UploadImg();       
+                    break;
                 default:
                     break;
             }
@@ -32,9 +39,89 @@ namespace Demo
             context.Response.End();
         }
 
+        private static void UploadImg()
+        {
+            var errorMsg = string.Empty ;
+            var httpContext = HttpContext.Current;
+            var files = httpContext.Request.Files;
+            if (files.Count == 0)
+            {
+                //todo:return error
+                errorMsg = "NO FILE";
+            }
+            var file = files[0];
+            var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+            var ext = Path.GetExtension(file.FileName);
+            if (string.IsNullOrEmpty(ext))
+            {
+                errorMsg = "UNKOWN EXTENSION";
+            }
+
+            //判斷檔案是否為圖檔
+            ext = ext.ToLower();
+
+            if (".jpg.jpeg.gif.png".IndexOf(ext, StringComparison.Ordinal) == -1)
+            {
+                errorMsg = "MUST BE .JPG .JPEG .GIF .PNG";
+            }
+
+            #region 判斷圖片size是否有合乎規格
+            var image = System.Drawing.Image.FromStream(file.InputStream);
+            //var width = image.Width;
+            //var height = image.Height;
+            //var range = ((double)height / (double)width);
+            //var isValidate = range <= 2 && range >= 0.5;
+            string code;
+            //if (!isValidate)
+            //{
+            //    code = string.Format("<script type='text/javascript'>new top.Dialog().Show('圖片的長除以寬不得大於2或小於0.5!');</script>");
+
+            //    httpContext.Response.Write(code);
+            //    httpContext.Response.End();
+            //    return;
+            //}
+            #endregion
+
+            //取得新檔名
+            fileName = string.Format("{0}{1}{2}", fileName, DateTime.Now.Ticks, ext);
+
+            #region 降低圖片品質(縮小檔案大小)
+            ImageCodecInfo jpegCodec = null;
+            // 指定圖片品質
+            var quality = new EncoderParameter(Encoder.Quality, 60L);
+
+            var codecs = ImageCodecInfo.GetImageEncoders();
+            var epParameters = new EncoderParameters(1);
+            epParameters.Param[0] = quality;
+            for (var i = 0; i < codecs.Length; i++)
+            {
+                if (codecs[i].MimeType != "image/jpeg") continue;
+
+                jpegCodec = codecs[i];
+                break;
+            }
+
+            #endregion
+            var dicPath = httpContext.Server.MapPath(string.Format(@"{0}\Image\Temp\", AppPath));
+            var path = string.Format("{0}{1}", dicPath, fileName);
+
+            var dicInfo = new DirectoryInfo(dicPath);
+            if (!dicInfo.Exists)
+            {
+                dicInfo.Create();
+            }
+            file.SaveAs(path);
+            //image = ScaleImage(image, 219);
+            image.Save(path, jpegCodec, epParameters);
+            code = string.Format("<script type='text/javascript'>new parent.SetExchangeItem().SetImage('{0}',true);</script>", fileName);
+
+            httpContext.Response.Write(code);
+            httpContext.Response.End();
+        }
+
         private static object GetPagerData()
         {
-            var physicalPath = string.Format(@"{0}\App_Code\Demo\pagerdata.json", HttpContext.Current.Request.MapPath(AppPath));
+            var physicalPath = string.Format(@"{0}\App_Code\Demo\pagerdata.json", HttpContext.Current.Server.MapPath(AppPath));
             var page = new Pager(physicalPath) ;
             
             int targetPage;
--- CMWebApp/DemoUpload.aspx
+++ CMWebApp/DemoUpload.aspx
@@ -3,7 +3,33 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" Runat="Server">
+    <input type="button" value="upload" id="uploadImg" />
+    <iframe id='iframeUpload' ></iframe>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="FootContent" Runat="Server">
+    <script type="text/javascript">
+        $(function () {
+            setEvent();
+
+
+            function setEvent() {
+                uploadImage();
+            }
+
+
+            function uploadImage() {//上傳圖片
+                $('#uploadImg').click(function() {
+                    var codeIfm;
+                    codeIfm = '<form enctype="multipart/form-data" action="/gunner/cmwebapp/ashx/demohandler.ashx" method="post">';
+                    codeIfm += '<input type="text" name="action" value="uploadimg">';
+                    codeIfm += '<input type="file" id="file" name="file" value="" onchange="document.forms[0].submit();return false;" />';
+                    codeIfm += '</form>';
+
+                    $('#iframeUpload').contents().find('html').html(codeIfm).find('#file').click();
+                });
+            }
+
+        });
+    </script>
 </asp:Content>
 
